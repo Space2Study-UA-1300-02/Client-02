423133f799b3012fbf17539493066738b2a516b9
Anna <anna2013novikova@gmail.com>
2025-01-27 15:33:30 +0200

    added test,s coverage 85.71

--- src/tests/unit/components/enhanced-table/enhanced-table-row/EnhancedTableRow.spec.jsx
+++ src/tests/unit/components/enhanced-table/enhanced-table-row/EnhancedTableRow.spec.jsx
@@ -34,7 +34,19 @@ const mockedCommonProps = {
 }
 
 describe('EnhancedTableRow component', () => {
-  beforeEach(() => {
+  // beforeEach(() => {
+  //   render(
+  //     <table>
+  //       <tbody>
+  //         <EnhancedTableRow {...mockedCommonProps} />
+  //       </tbody>
+  //     </table>
+  //   )
+  // })
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+  it('should render table row with correct data', () => {
     render(
       <table>
         <tbody>
@@ -42,26 +54,42 @@ describe('EnhancedTableRow component', () => {
         </tbody>
       </table>
     )
-  })
-  afterEach(() => {
-    vi.clearAllMocks()
-  })
-  it('should render table row with correct data', () => {
     expect(screen.getByText('John Doe')).toBeInTheDocument()
     expect(screen.getByText('john30@gmail.com')).toBeInTheDocument()
   })
   it('should call handleSelectClick when checkbox is clicked', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} />
+        </tbody>
+      </table>
+    )
     const checkbox = screen.getByRole('checkbox')
     await userEvent.click(checkbox)
     expect(mockedCommonProps.select.handleSelectClick).toHaveBeenCalledTimes(1)
   })
   it('should render action menu when menu icon is clicked', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} />
+        </tbody>
+      </table>
+    )
     const menuIcon = screen.getByTestId('menu-icon')
     await userEvent.click(menuIcon)
     const actionMenu = screen.getByRole('menu')
     expect(actionMenu).toBeInTheDocument()
   })
   it('should call onAction function when clicking on the menu item', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} />
+        </tbody>
+      </table>
+    )
     await userEvent.click(screen.getByTestId('menu-icon'))
     const menuItem = screen.getByText('Delete')
     await userEvent.click(menuItem)
@@ -70,9 +98,53 @@ describe('EnhancedTableRow component', () => {
     )
   })
   it('should close menu when "escape" is pressed', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} />
+        </tbody>
+      </table>
+    )
     const menuIcon = screen.getByTestId('menu-icon')
     await userEvent.click(menuIcon)
     await userEvent.keyboard('{Esc}')
     expect(screen.queryByRole('menu')).not.toBeInTheDocument()
+  })
+  it('should call onRowClick when the row is clicked', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} />
+        </tbody>
+      </table>
+    )
+    const row = screen.getByRole('row')
+    await userEvent.click(row)
+    expect(mockedCommonProps.onRowClick).toHaveBeenCalledWith(
+      mockedCommonProps.item
+    )
+  })
+  it('should render without rowActions', () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} rowActions={null} />
+        </tbody>
+      </table>
+    )
+    expect(screen.queryByTestId('menu-icon')).not.toBeInTheDocument()
+  })
+
+  it('should render without onRowClick', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...mockedCommonProps} onRowClick={null} />
+        </tbody>
+      </table>
+    )
+    const row = screen.getByRole('row')
+    await userEvent.click(row)
+    expect(mockedCommonProps.onRowClick).not.toHaveBeenCalled()
   })
 })
