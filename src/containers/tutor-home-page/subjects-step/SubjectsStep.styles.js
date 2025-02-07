9fb02c458acb09bedc3b81ab071f8809f9fbfd65
ANOVIKOVA21 <82145323+ANOVIKOVA21@users.noreply.github.com>
2025-02-07 16:02:17 +0200

    Feature/83/subject stepper (#29)
    * added Subjects Step and styles for it
    
    * fix styles

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
@@ -4,9 +4,36 @@ export const styles = {
   container: {
     display: 'flex',
     justifyContent: 'space-between',
-    gap: '40px',
+    alignItems: { md: 'normal', xs: 'center' },
+    flexDirection: { md: 'row', xs: 'column' },
+    gap: { md: '40px', sm: '0' },
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0px' },
     ...fadeAnimation
+  },
+  imgContainer: {
+    display: 'flex',
+    flex: 1,
+    maxWidth: { md: '432px', sm: '300px', xs: '300px' },
+    aspectRatio: { xs: '4/3', sm: 'auto' },
+    pb: { xs: '16px', sm: '52px' }
+  },
+  img: {
+    width: '100%',
+    m: { sm: 0, xs: '0 auto' }
+  },
+  fieldContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px',
+    mt: { md: '4px', xs: '20px' }
+  },
+  rigthBox: {
+    maxWidth: '432px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'start',
+    m: { md: 0, xs: '0 auto' },
+    pt: 0
   }
 }
--- src/containers/tutor-home-page/subjects-step/constants.js
+++ src/containers/tutor-home-page/subjects-step/constants.js
@@ -1,33 +1,60 @@
 export const categoriesMock = [
-  { name: 'Languages' },
-  { name: 'Mathematics' },
-  { name: 'Computer science' },
-  { name: 'Music' },
-  { name: 'History' }
+  { label: 'Languages' },
+  { label: 'Mathematics' },
+  { label: 'Computer science' },
+  { label: 'Music' },
+  { label: 'History' }
 ]
 
 export const languagesMock = [
-  { name: 'Chinese' },
-  { name: 'Czech' },
-  { name: 'Danish' },
-  { name: 'Dutch' },
-  { name: 'English' },
-  { name: 'Estonian' },
-  { name: 'Finnish' },
-  { name: 'French' },
-  { name: 'German' },
-  { name: 'Hungarian' },
-  { name: 'Icelandic' },
-  { name: 'Italian' },
-  { name: 'Japanese' },
-  { name: 'Korean' },
-  { name: 'Norwegian' },
-  { name: 'Polish' },
-  { name: 'Portuguese (Brazil)' },
-  { name: 'Portuguese (Portugal)' },
-  { name: 'Romanian' },
-  { name: 'Slovak' },
-  { name: 'Spanish' },
-  { name: 'Swedish' },
-  { name: 'Ukrainian' }
+  { label: 'Chinese' },
+  { label: 'Czech' },
+  { label: 'Danish' },
+  { label: 'Dutch' },
+  { label: 'English' },
+  { label: 'Estonian' },
+  { label: 'Finnish' },
+  { label: 'French' },
+  { label: 'German' },
+  { label: 'Hungarian' },
+  { label: 'Icelandic' },
+  { label: 'Italian' },
+  { label: 'Japanese' },
+  { label: 'Korean' },
+  { label: 'Norwegian' },
+  { label: 'Polish' },
+  { label: 'Portuguese (Brazil)' },
+  { label: 'Portuguese (Portugal)' },
+  { label: 'Romanian' },
+  { label: 'Slovak' },
+  { label: 'Spanish' },
+  { label: 'Swedish' },
+  { label: 'Ukrainian' }
 ]
+export const subjectsMock = {
+  Languages: languagesMock,
+  Mathematics: [
+    { label: 'Algebra' },
+    { label: 'Geometry' },
+    { label: 'Trigonometry' },
+    { label: 'Statistics' }
+  ],
+  'Computer science': [
+    { label: 'Java Script' },
+    { label: 'Java' },
+    { label: 'Ruby' },
+    { label: 'C#' }
+  ],
+  Music: [
+    { label: 'Musical Forms' },
+    { label: 'Vocal Practical' },
+    { label: 'Music Theory' },
+    { label: 'Solfege' }
+  ],
+  History: [
+    { label: 'Ancient History' },
+    { label: 'Medieval History' },
+    { label: 'Modern History' },
+    { label: 'History of Ukraine' }
+  ]
+}
