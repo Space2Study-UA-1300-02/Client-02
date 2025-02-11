d5f18e669280b22bb85006d42a7f5b9b024373f5
ZerkalAndrii <andriyoptimalv@gmail.com>
2025-02-11 14:50:56 +0200

    categories page filling

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,72 @@
-import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { Box, TextField, Button, Link, Typography } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import { styles } from '~/pages/categories/Categories.styles'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  const [query, setQuery] = useState('')
+  const navigate = useNavigate()
+
+  const handleSearch = () => {
+    if (query.trim()) {
+      navigate(`/find-offers?search=${encodeURIComponent(query)}`)
+    }
+  }
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch()
+    }
+  }
+  const { t } = useTranslation()
+
+  return (
+    <Box sx={styles.container}>
+      <Typography gutterBottom sx={styles.heading} variant='h4'>
+        {t('common.categoriestitle')}
+      </Typography>
+      <Typography gutterBottom sx={styles.subtitle} variant='subtitle1'>
+        {t('common.description')}
+      </Typography>
+      <Box sx={styles.linkWrapper}>
+        <Link
+          component='button'
+          onClick={() => navigate('/find-offers')}
+          variant='body2'
+        >
+          {t('common.showAllOffers')} &rarr;
+        </Link>
+      </Box>
+      <Box sx={styles.searchBox}>
+        <SearchIcon sx={styles.searchIcon} />
+        <TextField
+          InputProps={{ disableUnderline: true }}
+          fullWidth
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder={t('common.learningLabel')}
+          sx={styles.searchInput}
+          value={query}
+          variant='standard'
+        />
+        <Button
+          onClick={handleSearch}
+          sx={styles.searchButton}
+          variant={styles.searchButton.variant}
+        >
+          {t('common.search')}
+        </Button>
+      </Box>
+      <Typography sx={styles.infoText} variant='body2'>
+        Can&apos;t find what you&apos;re looking for? Request a new{' '}
+        <Link component='button'>{t('common.category')}</Link>
+        or
+        <Link component='button'>{t('common.subject')}</Link>!
+      </Typography>
+    </Box>
+  )
 }
 
 export default Categories
