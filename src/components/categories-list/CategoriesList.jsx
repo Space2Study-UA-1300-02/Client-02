b20b6e9b8806798c2072999a2168a7845fa13139
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-02-13 01:44:51 +0200

    implemented a component with a list of categories

---
+++ src/components/categories-list/CategoriesList.jsx
@@ -0,0 +1,45 @@
+import useAxios from '~/hooks/use-axios'
+import CardsList from '~/components/cards-list/CardsList'
+import { categoryService } from '~/services/category-service'
+import { useTranslation } from 'react-i18next'
+import { Box, Typography } from '@mui/material'
+import icon from '../../assets/img/student-home-page/service_icon.png'
+
+import { styles } from '~/components/categories-list/CategoriesList.styles'
+
+const CategoriesList = () => {
+  // const [limit, setLimit] = useState(12)
+
+  const { t } = useTranslation()
+
+  const { response, loading } = useAxios({
+    service: categoryService.getCategories,
+    defaultResponse: []
+  })
+
+  const categories = response.map((category) => (
+    <Box key={category._id} sx={styles.card}>
+      <Box sx={styles.iconContainer}>
+        <img alt={category.name} height={62} src={icon} width={62} />
+      </Box>
+
+      <Box sx={styles.textContainer}>
+        <Typography sx={styles.title}>{category.name}</Typography>
+        <Typography sx={styles.subtitle}>234 Offers</Typography>
+      </Box>
+    </Box>
+  ))
+
+  return (
+    <div style={{ marginTop: '30px', marginBottom: '100px' }}>
+      <CardsList
+        btnText={t('categoriesPage.viewMore')}
+        cards={categories}
+        isExpandable
+        loading={loading}
+      />
+    </div>
+  )
+}
+
+export default CategoriesList
