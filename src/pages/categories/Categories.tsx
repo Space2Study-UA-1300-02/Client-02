f17a8400b8f9bb297f96569dd87ba0d6d2844246
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-02-18 00:17:17 +0200

    refactoring of the category page, removed unnecessary component 'CategoriesList', corrected styles on the page

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { Box, TextField, Button, Link, Typography } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 import { styles } from '~/pages/categories/Categories.styles'
-import CategoriesList from '~/components/categories-list/CategoriesList'
+import useAxios from '~/hooks/use-axios'
+import { categoryService } from '~/services/category-service'
+import { CategoryInterface, SizeEnum } from '~/types'
+import CardWithLink from '~/components/card-with-link/CardWithLink'
+import { authRoutes } from '~/router/constants/authRoutes'
+import CardsList from '~/components/cards-list/CardsList'
+import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+import DirectionLink from '~/components/direction-link/DirectionLink'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import OfferRequestBlock from '~/containers/find-offer/offer-request-block/OfferRequestBlock'
 
 const Categories = () => {
   const [query, setQuery] = useState('')
   const navigate = useNavigate()
+
+  const { t } = useTranslation()
+
+  const { response: categories, loading } = useAxios({
+    service: categoryService.getCategories,
+    defaultResponse: []
+  })
 
   const handleSearch = () => {
     if (query.trim()) {
@@ -21,25 +38,41 @@ const Categories = () => {
       handleSearch()
     }
   }
-  const { t } = useTranslation()
+
+  const cards = useMemo(
+    () =>
+      categories.map((item: CategoryInterface) => {
+        return (
+          <CardWithLink
+            description={`235 ${t('categoriesPage.offers')}`}
+            img={item.appearance.icon}
+            key={item._id}
+            link={`${authRoutes.subjects.path}?categoryId=${item._id}`}
+            title={item.name}
+          />
+        )
+      }),
+    [categories, t]
+  )
 
   return (
-    <Box sx={styles.container}>
-      <Typography gutterBottom sx={styles.heading} variant='h4'>
-        {t('common.categoriestitle')}
-      </Typography>
-      <Typography gutterBottom sx={styles.subtitle} variant='subtitle1'>
-        {t('common.description')}
-      </Typography>
-      <Box sx={styles.linkWrapper}>
-        <Link
-          component='button'
-          onClick={() => navigate('/find-offers')}
-          variant='body2'
-        >
-          {t('common.showAllOffers')} &rarr;
-        </Link>
+    <PageWrapper>
+      <OfferRequestBlock />
+
+      <TitleWithDescription
+        description={t('common.description')}
+        style={styles.titleWithDescription}
+        title={t('common.categoriestitle')}
+      />
+
+      <Box sx={styles.navigation}>
+        <DirectionLink
+          after={<ArrowForwardIcon fontSize={SizeEnum.Small} />}
+          linkTo={authRoutes.findOffers.path}
+          title={t('subjectsPage.subjects.showAllOffers')}
+        />
       </Box>
+
       <Box sx={styles.searchBox}>
         <SearchIcon sx={styles.searchIcon} />
         <TextField
@@ -60,14 +93,22 @@ const Categories = () => {
           {t('common.search')}
         </Button>
       </Box>
+
       <Typography sx={styles.infoText} variant='body2'>
         Can&apos;t find what you&apos;re looking for? Request a new{' '}
         <Link component='button'>{t('common.category')}</Link>
         or
         <Link component='button'>{t('common.subject')}</Link>!
       </Typography>
-      <CategoriesList />
-    </Box>
+
+      <CardsList
+        btnText={t('categoriesPage.viewMore')}
+        cards={cards}
+        isExpandable
+        loading={loading}
+        onClick={() => {}}
+      />
+    </PageWrapper>
   )
 }
 
